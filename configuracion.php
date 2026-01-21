0da36b083f1ce67a9dcd7cf225bb2f3f812fad06
Nico <nicolas.bucarey@est.fi.uncoma.edu.ar>
2026-01-21 02:49:40 -0300

    Fix: Incluir todas las clases ABM y modelo necesarias

--- configuracion.php
+++ configuracion.php
@@ -16,9 +16,29 @@ include_once($ROOT.'util/funciones.php');
 
 // Incluir clases de control
 include_once($ROOT.'control/Session.php');
+include_once($ROOT.'control/AbmProducto.php');
+include_once($ROOT.'control/AbmUsuario.php');
+include_once($ROOT.'control/AbmRol.php');
+include_once($ROOT.'control/AbmCompra.php');
+include_once($ROOT.'control/AbmCompraItem.php');
+include_once($ROOT.'control/AbmCompraEstado.php');
+include_once($ROOT.'control/AbmMenu.php');
+include_once($ROOT.'control/AbmMenuRol.php');
+include_once($ROOT.'control/AbmUsuarioRol.php');
+include_once($ROOT.'control/AbmCompraEstadoTipo.php');
 
 // Incluir clases de modelo
 include_once($ROOT.'modelo/conector/BaseDatos.php');
+include_once($ROOT.'modelo/Producto.php');
+include_once($ROOT.'modelo/Usuario.php');
+include_once($ROOT.'modelo/Rol.php');
+include_once($ROOT.'modelo/Compra.php');
+include_once($ROOT.'modelo/CompraItem.php');
+include_once($ROOT.'modelo/CompraEstado.php');
+include_once($ROOT.'modelo/CompraEstadoTipo.php');
+include_once($ROOT.'modelo/Menu.php');
+include_once($ROOT.'modelo/MenuRol.php');
+include_once($ROOT.'modelo/UsuarioRol.php');
 
 // Variable que define la pagina de autenticacion del proyecto
 $host = $_SERVER['HTTP_HOST'];
